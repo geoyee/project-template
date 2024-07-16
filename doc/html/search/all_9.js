041884517f68a1a0d5edbf9358611aa37e908a65
geoyee <geoyee@yeah.net>
2024-07-16 09:05:34 +0800

    docs: Update doc

--- doc/html/search/all_9.js
+++ doc/html/search/all_9.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['point_0',['Point',['../structlibrary_t_1_1utiles_1_1_point.html',1,'libraryT::utiles']]],
   ['precision_1',['PRECISION',['../utiles_8cpp.html#a9c7b069fee3c8184e14a7de8e5da2dc6',1,'utiles.cpp']]],
-  ['printcallback_2',['PrintCallback',['../namespacelibrary_t_1_1utiles.html#a693a4d01077c06b2f0c8b7ce6dc40dd7',1,'libraryT::utiles']]]
+  ['printcallback_2',['PrintCallback',['../namespacelibrary_t_1_1utiles.html#aa34fd00abc144a846bf3459591cbecd1',1,'libraryT::utiles']]]
 ];
--- doc/html/search/typedefs_0.js
+++ doc/html/search/typedefs_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['printcallback_0',['PrintCallback',['../namespacelibrary_t_1_1utiles.html#a693a4d01077c06b2f0c8b7ce6dc40dd7',1,'libraryT::utiles']]]
+  ['printcallback_0',['PrintCallback',['../namespacelibrary_t_1_1utiles.html#aa34fd00abc144a846bf3459591cbecd1',1,'libraryT::utiles']]]
 ];
