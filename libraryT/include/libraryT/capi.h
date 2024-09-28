9545e825dc6d16377985d1c42d9de00d33665eba
geoyee <geoyee@yeah.net>
2024-09-28 11:54:14 +0800

    feat: Add spdlog

--- libraryT/include/libraryT/capi.h
+++ libraryT/include/libraryT/capi.h
@@ -7,65 +7,65 @@
 
 extern "C"
 {
-    using namespace libraryT::utiles;
+using namespace libraryT::utiles;
 
-    /**
-     * @brief 计算两个整数的加法
-     * @param x 第一个整数
-     * @param y 第二个整数
-     * @return 返回两个整数的和
-     */
-    int LIB_API MathAdd(int x, int y);
+/**
+ * @brief 计算两个整数的加法
+ * @param x 第一个整数
+ * @param y 第二个整数
+ * @return 返回两个整数的和
+ */
+int LIB_API MathAdd(int x, int y);
 
-    /**
-     * @brief 计算两个点间的距离
-     * @param p1 第一个点
-     * @param p2 第二个点
-     * @return 返回两个点间的距离
-     */
-    double LIB_API MathDistance(Point p1, Point p2);
-
-#ifdef SWIG
-    /**
-     * @brief 计算一个数的和
-     * @param arr 数组
-     * @return 返回和
-     */
-#else
-    /**
-     * @brief 计算一个数的和
-     * @param arr 数组
-     * @param size 数的个数
-     * @return 返回和
-     */
-#endif // SWIG
-    int LIB_API MathSum(int *arr, int size);
+/**
+ * @brief 计算两个点间的距离
+ * @param p1 第一个点
+ * @param p2 第二个点
+ * @return 返回两个点间的距离
+ */
+double LIB_API MathDistance(Point p1, Point p2);
 
 #ifdef SWIG
-    /**
-     * @brief 计算一个数哈希值
-     * @param x 需要计算的数
-     * @param callback 打印回调函数
-     * @return 返回哈希值
-     */
+/**
+ * @brief 计算一个数的和
+ * @param arr 数组
+ * @return 返回和
+ */
 #else
-    /**
-     * @brief 计算一个数哈希值
-     * @param x 需要计算的数
-     * @param callback 打印回调函数
-     * @param data 透传指针
-     * @return 返回哈希值
-     */
+/**
+ * @brief 计算一个数的和
+ * @param arr 数组
+ * @param size 数的个数
+ * @return 返回和
+ */
 #endif // SWIG
-    size_t LIB_API MathHash(double x, PrintCallback callback, void *data);
+int LIB_API MathSum(int *arr, int size);
 
-    /**
-     * @brief 计算一个数的开方
-     * @param x 开方数
-     * @param method 开方方法
-     * @return 返回开方结果
-     */
-    float LIB_API MathSqrt(float x, int method);
+#ifdef SWIG
+/**
+ * @brief 计算一个数哈希值
+ * @param x 需要计算的数
+ * @param callback 打印回调函数
+ * @return 返回哈希值
+ */
+#else
+/**
+ * @brief 计算一个数哈希值
+ * @param x 需要计算的数
+ * @param callback 打印回调函数
+ * @param data 透传指针
+ * @return 返回哈希值
+ */
+#endif // SWIG
+size_t LIB_API MathHash(double x, PrintCallback callback, void *data);
+
+/**
+ * @brief 计算一个数的开方
+ * @param x 开方数
+ * @param method 开方方法
+ * @return 返回开方结果
+ */
+float LIB_API MathSqrt(float x, int method);
 }
 
 #endif // LIBRARY_T_CAPI_H
